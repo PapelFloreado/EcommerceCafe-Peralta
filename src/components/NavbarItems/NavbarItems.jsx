49acf9e5070e96338a07a7dabc0317f561a318cd
Gabriel Peralta <pgabrieloscar@gmail.com>
2022-08-03 21:29:40 -0300

    Se agrego Bootstrap al proyecto V2

--- src/components/NavbarItems/NavbarItems.jsx
+++ src/components/NavbarItems/NavbarItems.jsx
@@ -4,23 +4,19 @@ import Navbar from 'react-bootstrap/Navbar';
 import "./NavbarItems.css"
 import NavDropdown from 'react-bootstrap/NavDropdown'
 
-const NavItems = ["Home", "Shop", "About", "Contact"]
-
 const NavBarItems = () =>{
     return (
         <Container className="d-flex justify-content-between">
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="d-flex justify-content-evenly nav-item">
-            {NavItems.map((item)=>(
-                <Nav.Link key={item} href="#home">{item}</Nav.Link>
-            ))}
-          <NavDropdown title={NavItems.Shop} id="basic-nav-dropdown">
-              <NavDropdown.Item href="#">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#">
-                Another action
-              </NavDropdown.Item>
-          </NavDropdown>
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Contacto</Nav.Link>
+            <NavDropdown className="nav-drop" title="Shop" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#">Café</NavDropdown.Item>
+                <NavDropdown.Item href="#">Accesorios</NavDropdown.Item>
+                <NavDropdown.Item href="#">Insumos</NavDropdown.Item>
+            </NavDropdown>
           </Nav>
           </Navbar.Collapse>
         </Container>
